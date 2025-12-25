1c5851bdb15e74199ec4d13952531bce9930d8f7
Gyul <159827931+Gyul123@users.noreply.github.com>
2025-12-25 19:58:09 +0100

    Update imagebuffer.py

--- imagebuffer.py
+++ imagebuffer.py
@@ -3,11 +3,11 @@ import busio
 import adafruit_ssd1306
 import adafruit_framebuf
 
-def print_buffer(the_fb):
+def print_buffer(the_fb, dp):
     for y in range(the_fb.height):
         for x in range(the_fb.width):
             if fb.pixel(x, y):
-                display.pixel(x, y, 1)
+                dp.pixel(x, y, 1)
             else:
                 pass
-    display.show()
+    dp.show()
